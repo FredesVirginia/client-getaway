462df126b0442edf7236da94475f4dd2c9e0bce2
Fredes Virginia <William-Champion@outlook.com>
2025-05-09 05:18:01 -0300

    cambios beyota

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { PRODUCT_SERVICE } from 'src/config';
+import { envs, PRODUCT_SERVICE } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
@@ -12,8 +12,8 @@ import { PRODUCT_SERVICE } from 'src/config';
         name : PRODUCT_SERVICE ,
         transport : Transport.TCP,
         options : {
-          host : "localhost",
-          port : 3001
+          host : envs.productMicroserviceHost,
+          port : envs.productMicroservicePort
         }
       }
     ])
